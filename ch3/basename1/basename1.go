63782ddefce36a7359ef6290af76fdf56fc6005f
roylee0704 <roylee0704@gmail.com>
2016-01-25 11:26:31 +0800

    complete ch3/basename1

---
+++ ch3/basename1/basename1.go
@@ -0,0 +1,32 @@
+// naive: basename removes directory components and a .suffix
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(basename("a/b/c.go")) // "c"
+	fmt.Println(basename("c.d.go"))   // "c.d"
+	fmt.Println(basename("a.go"))     // "a"
+	fmt.Println(basename("abc"))      // "abc"
+}
+
+func basename(s string) string {
+
+	// Discard last '/' and everything before
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+
+	// Preserve everything before last '.'
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+
+	return s
+}
