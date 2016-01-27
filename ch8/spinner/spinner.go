a9d5c6eff78d9e29fa79b2a8232744d62e705f6d
roylee0704 <roylee0704@gmail.com>
2016-01-27 12:28:28 +0800

    completed ch8/spinner

---
+++ ch8/spinner/spinner.go
@@ -0,0 +1,35 @@
+// Spinner provide the user with a visual indication that the program is still
+// running, by displaying an animated textual "spinner"
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond) // animate 0.1s per frame.
+
+	const n = 45
+	fibN := fib(n)
+	fmt.Printf("\rFibonnaci(%d)\n", fibN)
+}
+
+// fib computes fibonacci number at nth position. (in-efficient)
+func fib(n int) int {
+	if n <= 2 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r Crunching... %c", r)
+			time.Sleep(delay)
+		}
+	}
+
+}
