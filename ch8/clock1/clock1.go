2fc4f8a637ba6cfb8c30a258c26e687ba25d4d9f
roylee0704 <roylee0704@gmail.com>
2016-01-27 22:35:48 +0800

    completed clock1/

---
+++ ch8/clock1/clock1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	fmt.Println("haha")
+	listener, err := net.Listen("tcp", "localhost:8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) //e.g., connection aborted
+			//log.Fatal(err)
+
+			continue
+		}
+
+		handleConn(conn) //handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			return         // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
