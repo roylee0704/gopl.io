de147ba2c2ff712c85f85396549510c04e32368b
roylee0704 <roylee0704@gmail.com>
2016-02-28 22:05:10 +0800

    completed issuesreport, go text-template

---
+++ ch4/issuesreport/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/roylee0704/gopl.io/ch4/github"
+)
+
+//!+template
+const templ = `{{.TotalCount}} issues
+{{range .Items}}-----------------------
+Number: {{.Number}}
+User:   {{.User.Login}}
+Title:  {{.Title | printf "%.40s"}}
+Age:    {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+//!-template
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+var report = template.Must(template.New("issuelist").
+	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+	Parse(templ))
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
