5b5c90cca07c951fdd25ccaf0b92492a8447cbcc
roylee0704 <roylee0704@gmail.com>
2016-02-28 18:03:50 +0800

    complete exercise4.11:

--- ch4/issues/main.go
+++ ch4/issues/main.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/roylee0704/gopl.io/ch4/github"
 )
+
+type ageReport time.Duration
+
+func (a ageReport) String() string {
+
+	days := int(time.Duration(a).Hours() / 24)
+
+	var ret string
+	switch {
+	case days < 31:
+		ret = "< 1 month"
+	case days < 365:
+		ret = "< 1 year"
+	default:
+		ret = "> 1 year"
+	}
+	return ret
+}
 
 func main() {
 	result, err := github.SearchIssues(os.Args[1:])
@@ -17,8 +36,9 @@ func main() {
 	fmt.Printf("%d issues:\n", result.TotalCount)
 
 	for _, issue := range result.Items {
-		//%9.9s
-		fmt.Printf("#%-5d|%9.9s|%.55s\n", issue.Number, issue.User.Login, issue.Title)
+
+		var a ageReport = ageReport(time.Since(issue.CreatedAt))
+		fmt.Printf("#%-5d|%9s|%9.9s|%.55s\n", issue.Number, a, issue.User.Login, issue.Title)
 	}
 
 }
